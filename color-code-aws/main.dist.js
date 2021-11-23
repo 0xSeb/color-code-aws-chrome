0b4069a31e87e1f5f569ddba4987257bdb075540
Sebastien Marchal <smarchal@squareup.com>
2021-11-23 10:27:45 +0100

    Update with new aws console gui update + formatting

--- color-code-aws/main.dist.js
+++ color-code-aws/main.dist.js
@@ -1,46 +1,46 @@
 const prodAccount = {
-    number: '<account-number>',
-    title: '⚠️ PROD AWS',
-    color: 'red',
-    warningMessage: '⚠️ THIS IS PROD'
-  }
+	number: '<account-number>',
+	title: '⚠️ PROD AWS',
+	color: 'red',
+	warningMessage: '⚠️ THIS IS PROD'
+}
 
-  const stageAccount = {
-      number: '<account-number>',
-      title: 'ℹ️ STAGE AWS',
-      color: 'forestgreen',
-      warningMessage: 'ℹ️ THIS IS STAGE'
-  }
+const stageAccount = {
+	number: '<account-number>',
+	title: 'ℹ️ STAGE AWS',
+	color: 'forestgreen',
+	warningMessage: 'ℹ️ THIS IS STAGE'
+}
 
-  const accounts = [prodAccount, stageAccount]
+const accounts = [prodAccount, stageAccount]
 
-  init();
+init();
 
-  function init() {
-      const currentAccountNumber = document.querySelector('[data-testid="awsc-nav-account-menu-button"]').children[0].title;
-      const header = document.querySelector('[data-testid="awsc-nav-header-viewport-shelf-inner"]');
-      const footer = document.getElementById('console-nav-footer-inner');
-      const searchBar = document.querySelector('[data-testid="awsc-concierge-input-hint"]');
-      const seachBarInput = document.querySelector('[data-testid="awsc-concierge-input"]');
-      const welcomeBanner = document.querySelector('[data-testid="banner"]');
-      const logoAWS = document.querySelector('[data-testid="nav-logo"]');
-      const susLogo = document.createElement("img");
-      susLogo.src = chrome.runtime.getURL('suslogo.png');
-      susLogo.setAttribute('style', 'max-height: 20px');
+function init() {
+	const currentAccountNumber = document.querySelector('[data-testid="awsc-nav-account-menu-button"]').children[0].title;
+	const header = document.querySelector('[data-testid="awsc-nav-header-viewport-shelf-inner"]');
+	const footer = document.getElementById('console-nav-footer-inner');
+	const searchBar = document.querySelector('[data-testid="awsc-concierge-input-hint"]');
+	const seachBarInput = document.querySelector('[data-testid="awsc-concierge-input"]');
+	const welcomeBanner = document.querySelector('[data-testid="banner"]');
+	const logoAWS = document.querySelector('[data-testid="nav-logo"]');
+	const susLogo = document.createElement("img");
+	susLogo.src = chrome.runtime.getURL('suslogo.png');
+	susLogo.setAttribute('style', 'max-height: 20px');
 
-      const isCurrentAccount = (element) => currentAccountNumber.includes(element.number);
-      const accountIndex = accounts.findIndex(isCurrentAccount);
-      if(accountIndex > -1){
-          document.title = accounts[accountIndex].title;
-          if(header){header.setAttribute('style', `background-color: ${accounts[accountIndex].color} !important`)};
-          if(footer){footer.setAttribute('style', `background-color: ${accounts[accountIndex].color} !important`)};
-          if(searchBar){searchBar.setAttribute('style', `background-color: ${accounts[accountIndex].color} !important`)};
-          if(seachBarInput){seachBarInput.setAttribute('placeholder', accounts[accountIndex].warningMessage)};
-          if(welcomeBanner){welcomeBanner.innerHTML = accounts[accountIndex].warningMessage};
-          if(logoAWS){
-              logoAWS.children[0].remove();
-              logoAWS.appendChild(susLogo);
-          }
+	const isCurrentAccount = (element) => currentAccountNumber.includes(element.number);
+	const accountIndex = accounts.findIndex(isCurrentAccount);
+	if (accountIndex > -1) {
+		document.title = accounts[accountIndex].title;
+		if (header) {header.setAttribute('style', `background-color: ${accounts[accountIndex].color} !important`)};
+		if (footer) {footer.setAttribute('style', `background-color: ${accounts[accountIndex].color} !important`)};
+		if (searchBar) {searchBar.setAttribute('style', `background-color: ${accounts[accountIndex].color} !important`)};
+		if (seachBarInput) {seachBarInput.setAttribute('placeholder', accounts[accountIndex].warningMessage)};
+		if (welcomeBanner) {welcomeBanner.innerHTML = accounts[accountIndex].warningMessage	};
+		if (logoAWS) {
+			logoAWS.children[0].remove();
+			logoAWS.appendChild(susLogo);
+		}
 
-      }
-  }
+	}
+}
